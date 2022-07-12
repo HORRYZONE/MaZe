e9d8cef6aeae4a14c74a1d3e9db08332b918fc65
HORRYZONE <69580122+HORRYZONE@users.noreply.github.com>
2022-07-13 00:55:26 +0800

    Update maze.java

--- maze.java
+++ maze.java
@@ -5,24 +5,42 @@ import java.util.Random;
 public class test2 {
     static LinkedList<position> path = new LinkedList<position>();
     static ArrayList<String> option = new ArrayList<String>();
-    static int [][] maze =  new int[4][4];
+    static int [][] maze = new int[4][4];
 
 
     public static void main(String[] args) {
 
-        while (true){
-            generateMaze();
+        int count = 0;
+        boolean done = false;
+        position p = new position(0,0);
+        path.push(p);
+do {
+     //start position
+    int y = path.peek().y;  //check value for y in position
+    int x = path.peek().x;
+    maze[y][x] = 1;
+
+    generateMaze(y,x);
+    option.clear();
+    count++;
+
+    if (count == 15)
+    {
+        break;
+    }
+}while (done != true);
+        {
+            System.out.println("completed");
         }
+
+
+
+
 
     }
 
-    public static void generateMaze()
+    public static void generateMaze(int y, int x)
     {
-        position p = new position(0,0);
-        path.push(p); //start position
-         int y = path.peek().y;  //check value for y in position
-         int x = path.peek().x;
-        maze[y][x] = 1;
 
         checkDirection(y,x);
         String SelectedPath = RandomPath();
@@ -31,35 +49,37 @@ public class test2 {
 
     public static void checkDirection(int y, int x)
     {
-        boolean decision = false;
+        boolean havePath = false;
 
-        do {
+
             if (isValid(y-1,x) == true && isNull(y-1,x) == true)
             {
                 option.add("up");
+                havePath = true;
+
             }
              if (isValid(y+1,x) == true && isNull(y+1,x) == true)
             {
                 option.add("down");
+                havePath = true;
             }
              if (isValid(y,x-1) == true && isNull(y,x-1) == true)
             {
                 option.add("left");
+                havePath = true;
             }
              if (isValid(y,x+1) == true && isNull(y,x+1) == true)
             {
                 option.add("right");
+                havePath = true;
             }
-             
 
-            decision = true;
+             if(havePath == false){
+                 option.add("none");
+             }
 
-
-        }while (decision != true);
-        {
             System.out.println(option);
 
-        }
 
     }
 
@@ -113,7 +133,7 @@ public class test2 {
 
     public static boolean isValid(int y, int x)
     {
-        if (y < 0 ||  x < 0 || y >= 4 || x >= 4)   //length is not zero base so the length is 3
+        if (y < 0 ||  x < 0 || y >= maze.length || x >= maze[y].length)   //length is not zero base so the length is 3
         {
             System.out.println("out bound\n");
             return false;
